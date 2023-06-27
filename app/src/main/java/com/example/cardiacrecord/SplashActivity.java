bfb876df77d5875c66ded0ee83498f5552c93cd4
kazol196295 <kazol196295@gmail.com>
2023-06-27 14:45:18 +0600

    delay 3 seconds for splash

--- app/src/main/java/com/example/cardiacrecord/SplashActivity.java
+++ app/src/main/java/com/example/cardiacrecord/SplashActivity.java
@@ -14,7 +14,7 @@ import com.bumptech.glide.Glide;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final long SPLASH_DELAY = 2000; // 2 seconds
+    private static final long SPLASH_DELAY = 3000; // 2 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
