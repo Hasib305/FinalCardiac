421e5482a8b4e49f5950a9963ef96bfb73e5525f
Hasib305 <hasibhasan305@gmail.com>
2023-07-02 00:26:00 +0600

    javadoc comment completed

--- app/src/main/java/com/example/cardiacrecord/UserRVModal.java
+++ app/src/main/java/com/example/cardiacrecord/UserRVModal.java
@@ -5,19 +5,24 @@ import android.os.Parcelable;
 
 import androidx.annotation.NonNull;
 
+/**
+ * The UserRVModal class represents a user's record displayed in the RecyclerView.
+ * It implements the Parcelable interface to enable passing data between activities.
+ */
 public class UserRVModal implements Parcelable {
-    private String userName ;
-    private  String userDesc;
-    private  String usersys ;
-    private  String userdio ;
-    private  String userheart;
-    private String userId ;
+    private String userName;
+    private String userDesc;
+    private String usersys;
+    private String userdio;
+    private String userheart;
+    private String userId;
     private String userdate;
     private String usertime;
 
-
-    public UserRVModal(){
-
+    /**
+     * Default constructor for the UserRVModal class.
+     */
+    public UserRVModal() {
     }
 
     protected UserRVModal(Parcel in) {
@@ -43,70 +48,162 @@ public class UserRVModal implements Parcelable {
         }
     };
 
+    /**
+     * Retrieves the user's name.
+     *
+     * @return The user's name.
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Sets the user's name.
+     *
+     * @param userName The user's name to set.
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * Retrieves the user's description.
+     *
+     * @return The user's description.
+     */
     public String getUserDesc() {
         return userDesc;
     }
 
+    /**
+     * Sets the user's description.
+     *
+     * @param userDesc The user's description to set.
+     */
     public void setUserDesc(String userDesc) {
         this.userDesc = userDesc;
     }
 
+    /**
+     * Retrieves the user's systolic pressure.
+     *
+     * @return The user's systolic pressure.
+     */
     public String getUsersys() {
         return usersys;
     }
 
+    /**
+     * Sets the user's systolic pressure.
+     *
+     * @param usersys The user's systolic pressure to set.
+     */
     public void setUsersys(String usersys) {
         this.usersys = usersys;
     }
 
+    /**
+     * Retrieves the user's diastolic pressure.
+     *
+     * @return The user's diastolic pressure.
+     */
     public String getUserdio() {
         return userdio;
     }
 
+    /**
+     * Sets the user's diastolic pressure.
+     *
+     * @param userdio The user's diastolic pressure to set.
+     */
     public void setUserdio(String userdio) {
         this.userdio = userdio;
     }
 
+    /**
+     * Retrieves the user's heart rate.
+     *
+     * @return The user's heart rate.
+     */
     public String getUserheart() {
         return userheart;
     }
 
+    /**
+     * Sets the user's heart rate.
+     *
+     * @param userheart The user's heart rate to set.
+     */
     public void setUserheart(String userheart) {
         this.userheart = userheart;
     }
 
+    /**
+     * Retrieves the user's ID.
+     *
+     * @return The user's ID.
+     */
     public String getUserId() {
         return userId;
     }
 
+    /**
+     * Sets the user's ID.
+     *
+     * @param userId The user's ID to set.
+     */
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    /**
+     * Retrieves the user's date.
+     *
+     * @return The user's date.
+     */
     public String getUserdate() {
         return userdate;
     }
 
+    /**
+     * Sets the user's date.
+     *
+     * @param userdate The user's date to set.
+     */
     public void setUserdate(String userdate) {
         this.userdate = userdate;
     }
 
+    /**
+     * Retrieves the user's time.
+     *
+     * @return The user's time.
+     */
     public String getUsertime() {
         return usertime;
     }
 
+    /**
+     * Sets the user's time.
+     *
+     * @param usertime The user's time to set.
+     */
     public void setUsertime(String usertime) {
         this.usertime = usertime;
     }
 
+    /**
+     * Constructor for the UserRVModal class.
+     *
+     * @param userName  The user's name.
+     * @param userDesc  The user's description.
+     * @param usersys   The user's systolic pressure.
+     * @param userdio   The user's diastolic pressure.
+     * @param userheart The user's heart rate.
+     * @param userId    The user's ID.
+     * @param userdate  The user's date.
+     * @param usertime  The user's time.
+     */
     public UserRVModal(String userName, String userDesc, String usersys, String userdio, String userheart, String userId, String userdate, String usertime) {
         this.userName = userName;
         this.userDesc = userDesc;
@@ -134,9 +231,14 @@ public class UserRVModal implements Parcelable {
         dest.writeString(userdate);
         dest.writeString(usertime);
     }
+
+    /**
+     * Compares the current UserRVModal object with another UserRVModal object based on usertime.
+     *
+     * @param measurement The UserRVModal object to compare.
+     * @return The comparison result.
+     */
     public int compareTo(UserRVModal measurement) {
         return this.usertime.compareTo(measurement.getUsertime());
     }
-
-
 }
